395e7662fa2261c01ad26f49d508959f18e2a24b
Nick Youngblut <nyoungb2@gmail.com>
2019-01-19 09:49:55 +0100

    Added E539; made table selection 2-tiered

--- table/server.R
+++ table/server.R
@@ -7,13 +7,20 @@ source("../utils/io.R")
 
 #db_dir = '/Volumes/abt3_projects/TwinsUK/Dataset_summary/metadata_db/'
 db_dir = '/ebio/abt3_projects/TwinsUK/Dataset_summary/metadata_db/'
+# basic metadata
 metadata_basic = file.path(db_dir, 'metadata_basic.tsv')
 metadata_collection = file.path(db_dir, 'metadata__collection.tsv')
 metadata_additional = file.path(db_dir, 'metadata_additional.tsv')
+# E539
+E539_immune = file.path(db_dir, 'E539_immune.tsv')
+E539_medication = file.path(db_dir, 'E539_medication.tsv')
+# E662
 E662_AS = file.path(db_dir, 'E662_AS.tsv')
 E662_raw = file.path(db_dir, 'E662_raw.tsv')
 E788_raw = file.path(db_dir, 'E788.tsv')
+# E788
 E788_key = file.path(db_dir, 'E788_key.tsv')
+# sequence data
 rRNA16S_qiime2 = file.path(db_dir, '16S_qiime2.tsv')
 metagenome = file.path(db_dir, 'metagenomes.tsv')
 twubif_bifido_capture = file.path(db_dir, 'twubif_bifido_capture.tsv')
@@ -35,52 +42,63 @@ shinyServer(function(input, output, session) {
     } 
     # table joins 
     ## extended basic metadata
-    if('show_collection' %in% input$tables){
+    if('show_collection' %in% input$tables_misc){
       x = x %>%
         left_join(read.delim(metadata_collection, sep='\t'),
                   c('s.FPBarcode'))
     } 
-    if('show_additional' %in% input$tables){
+    if('show_additional' %in% input$tables_misc){
       x = x %>%
         left_join(read.delim(metadata_additional, sep='\t'),
                   c('s.FPBarcode'))
     } 
+    ## E539
+    if('show_E539_immune' %in% input$tables_E539){
+      x = x %>%
+        left_join(read.delim(E539_immune, sep='\t'),
+                  c('i.IndividualID'))
+    } 
+    if('show_E539_medication' %in% input$tables_E539){
+      x = x %>%
+        left_join(read.delim(E539_medication, sep='\t'),
+                  c('i.IndividualID'))
+    } 
     ## E662 
-    if('show_E662_AS' %in% input$tables){
+    if('show_E662_AS' %in% input$tables_E662){
       x = x %>%
         left_join(read.delim(E662_AS, sep='\t'),
                   c('s.NameOnSampleWithAnon' = 'Microbiome_ID'))
     } 
-    if('show_E662_raw' %in% input$tables){
+    if('show_E662_raw' %in% input$tables_E662){
       x = x %>%
         left_join(read.delim(E662_raw, sep='\t'),
                   c('s.NameOnSampleWithAnon' = 'Microbiome_ID'))
     } 
     ## E788
-    if('show_E788_raw' %in% input$tables){
+    if('show_E788_raw' %in% input$tables_E788){
       x = x %>%
         left_join(read.delim(E788_raw, sep='\t'),
                   c('s.NameOnSampleWithAnon' = 'Microbiome_ID'))
     } 
-    if('show_E788_raw' %in% input$tables & 'show_E788_key' %in% input$tables){
+    if('show_E788_raw' %in% input$tables_E788 & 'show_E788_key' %in% input$tables_E788){
       x = x %>%
         left_join(read.delim(E788_key, sep='\t'),
                   c('E788_phenID' = 'PhenID',
                     'E788_phen_value' = 'Phen_value'))
     } 
     ## 16S
-    if('show_16S_qiime2' %in% input$tables){
+    if('show_16S_qiime2' %in% input$tables_seq){
       x = x %>%
         left_join(read.delim(rRNA16S_qiime2, sep='\t'),
                   c('s.FPBarcode' = 'FPBarcode'))
     } 
-    if('show_metagenome' %in% input$tables){
+    if('show_metagenome' %in% input$tables_seq){
       x = x %>%
         left_join(read.delim(metagenome, sep='\t'),
                   c('s.FPBarcode' = 'Sample'))
     } 
     ## twubif
-    if('show_twubif_capture' %in% input$tables){
+    if('show_twubif_capture' %in% input$tables_seq){
       x = x %>%
         left_join(read.delim(twubif_bifido_capture, sep='\t'),
                   c('s.FPBarcode' = 'Sample'))
--- table/ui.R
+++ table/ui.R
@@ -7,30 +7,63 @@ library(plotly)
 shinyUI(fluidPage(
   fluidRow(
     column(4,
+           downloadButton("downloadData", "Download selected data"),
+           hr(),
            fileInput('query_file', 
                      label = "File listing queries, 1 per line"),
            textInput('query_column', 
                      label = 'Which column to query? ("all" = all columns)',
                      value = 'all') 
     ),
-    column(4,
+    column(3,
            checkboxGroupInput('tables',
                               label = 'Data to show',
-                              choices = c('Show collection metadata?' = 'show_collection',
-                                          'Show additional metadata?' = 'show_additional',
-                                          'Show E662 anxiety scores?' = 'show_E662_AS',
-                                          'Show E662 raw data?' = 'show_E662_raw',
-                                          'Show E788 raw data?' = 'show_E788_raw',
-                                          'Show E788 data key?' = 'show_E788_key',
-                                          'Show 16S qiime2 data?' = 'show_16S_qiime2',
-                                          'Show metagenome data?' = 'show_metagenome',
-                                          'Show TuKBif Bifidobacterium capture reactions?' = 'show_twubif_capture')
-           )
+                              choices = c('Show misc metadata?' = 'show_misc',
+                                          'Show E662 metadata?' = 'show_E662',
+                                          'Show E788 metadata?' = 'show_E788',
+                                          'Show E539 metadata?' = 'show_E539',
+                                          'Show sequence data?' = 'show_seq')
+           ),
+           h5('WARNING: table joins on tables with many values will be slow')
     ),
-    column(2, 
-           br(),
-           br(),
-           downloadButton("downloadData", "Download selected data")
+    column(4, 
+           conditionalPanel(
+             'input.tables.includes("show_misc")',
+             checkboxGroupInput('tables_misc',
+                                label = 'Misc metadata',
+                                choices = c('Show collection metadata?' = 'show_collection',
+                                            'Show additional metadata?' = 'show_additional'))
+                                
+           ),
+           conditionalPanel(
+             'input.tables.includes("show_E662")',   
+             checkboxGroupInput('tables_E662',
+                                label = 'E662 metadata',
+                                choices = c('Show E662 anxiety scores?' = 'show_E662_AS',
+                                            'Show E662 raw data?' = 'show_E662_raw'))
+           ),
+           conditionalPanel(
+             'input.tables.includes("show_E788")',   
+             checkboxGroupInput('tables_E788',
+                                label = 'E788 metadata',
+                                choices = c('Show E788 raw data?' = 'show_E788_raw',
+                                            'Show E788 data key?' = 'show_E788_key'))
+           ),
+           conditionalPanel(
+             'input.tables.includes("show_E539")',   
+             checkboxGroupInput('tables_E539',
+                                label = 'E539 metadata',
+                                choices = c('Show E539 immune data?' = 'show_E539_immune',
+                                            'Show E539 medication data?' = 'show_E539_medication'))
+           ),
+           conditionalPanel(
+             'input.tables.includes("show_seq")',   
+             checkboxGroupInput('tables_seq',
+                                label = 'Sequence data',
+                                choices = c('Show 16S qiime2 data?' = 'show_16S_qiime2',
+                                            'Show metagenome data?' = 'show_metagenome',
+                                            'Show TuKBif Bifidobacterium capture reactions?' = 'show_twubif_capture'))
+           )
     )
   ),
   tags$hr(),
